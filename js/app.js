d7d517e39967e72fc74b51eb46f2db042b4753ae
biokah <maria.anaisr@gmail.com>
2018-01-27 23:50:43 -0600

    pantalla log-in y sign-up

---
+++ js/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('#log-in-button').click(logInFunction)
+    $('#sign-up-button').click(signUpFunction)
+    $('#user-button').click(validateUser)
+});
+
+function logInFunction() {
+    $('#first-view').hide()
+    $('#log-in').show()
+}
+
+function signUpFunction() {
+    $('#first-view').hide()
+    $('#sign-up').show()
+}
+
+function validateUser() {
+
+    if ($('#user').val() === 'frish.lin@laboratoria.la' && $('#password').val() === 'frish.lin@laboratoria.la') {
+        window.location.href = 'views/profile.html'
+    } else {
+        alert('Ingresa una cuenta válida')
+    }
+
+}
